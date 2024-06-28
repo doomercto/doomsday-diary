ef74a9d47d3f4545feb2e2dc182e5eaea20c4bc7
doomercto <173987414+doomercto@users.noreply.github.com>
2024-06-27 18:59:09 -0700

    update landing page

---
+++ app/page.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { Lacquer } from 'next/font/google';
+import Link from 'next/link';
+
+import { cn } from '@/lib/utils';
+
+const fontLacquer = Lacquer({
+  subsets: ['latin'],
+  variable: '--font-sans',
+  weight: '400',
+});
+
+export default function LandingPage() {
+  return (
+    <>
+      <div className="absolute h-screen w-screen">
+        <video
+          className="h-full w-full object-cover"
+          autoPlay
+          muted
+          loop
+          src="background.mp4"
+        />
+      </div>
+      <div
+        className={cn(
+          fontLacquer.variable,
+          'font-sans h-screen w-screen flex flex-col items-center justify-center'
+        )}
+      >
+        <div className="h-[60vh] max-sm:hidden" />
+        <Link
+          className="m-auto text-5xl text-slate-200 drop-shadow-[2px_2px_0px_theme(colors.slate.500)] opacity-80 hover:opacity-100 cursor-pointer"
+          href="/feed"
+        >
+          Enter
+        </Link>
+      </div>
+    </>
+  );
+}
