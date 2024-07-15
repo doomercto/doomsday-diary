10656eb5101374962639338344c826d638df5654
doomercto <173987414+doomercto@users.noreply.github.com>
2024-07-14 18:08:49 -0700

    add loading state for admin page

--- components/admin-post-list.tsx
+++ components/admin-post-list.tsx
@@ -91,7 +91,6 @@ export default function AdminPostList({
       {!posts.length && (
         <motion.div
           layout
-          initial={{ scale: 0.8, opacity: 0 }}
           animate={{ scale: 1, opacity: 1 }}
           exit={{ scale: 0.8, opacity: 0 }}
           transition={{ type: 'tween' }}
