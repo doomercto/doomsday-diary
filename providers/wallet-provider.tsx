ddbf22701428bfd0b13ef583a9409069eecb6eba
doomercto <173987414+doomercto@users.noreply.github.com>
2024-07-21 01:41:39 -0700

    initialize coinbase wallet sdk if not found

--- providers/wallet-provider.tsx
+++ providers/wallet-provider.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { createContext, useEffect, useState } from 'react';
+import CoinbaseWalletSDK from '@coinbase/wallet-sdk';
 
 import type { EthereumProvider } from '@/global';
 import type { ReactNode, ComponentType } from 'react';
@@ -16,17 +17,24 @@ export default function WalletProvider({ children }: { children: ReactNode }) {
   const [providers, setProviders] = useState<WalletProviders>({});
   useEffect(() => {
     const providersMap: WalletProviders = {};
-    if (!window?.ethereum) {
-      setProviders(providersMap);
-      return;
+    if (window?.ethereum) {
+      if (window.ethereum.providers) {
+        providersMap.cb = window.ethereum.providers.find(
+          p => p.isCoinbaseWallet
+        );
+        providersMap.mm = window.ethereum.providers.find(p => p.isMetaMask);
+      } else if (window.ethereum.isCoinbaseWallet) {
+        providersMap.cb = window.ethereum;
+      } else if (window.ethereum.isMetaMask) {
+        providersMap.mm = window.ethereum;
+      }
     }
-    if (window.ethereum.providers) {
-      providersMap.cb = window.ethereum.providers.find(p => p.isCoinbaseWallet);
-      providersMap.mm = window.ethereum.providers.find(p => p.isMetaMask);
-    } else if (window.ethereum.isCoinbaseWallet) {
-      providersMap.cb = window.ethereum;
-    } else if (window.ethereum.isMetaMask) {
-      providersMap.mm = window.ethereum;
+    if (!providersMap.cb) {
+      const cb = new CoinbaseWalletSDK({
+        appName: 'The Doomsday Diary',
+        appLogoUrl: `${location.origin}/icon.png`,
+      });
+      providersMap.cb = cb.makeWeb3Provider() as unknown as EthereumProvider;
     }
     setProviders(providersMap);
   }, []);
